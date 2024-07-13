ab3d71066146c864801072361feaa598fca3ac0a
Gustavo <a01972463@usu.edu>
2024-07-13 12:00:28 -0600

    TrackerViewHelper Private setters

--- src/main/kotlin/TrackerViewHelper.kt
+++ src/main/kotlin/TrackerViewHelper.kt
@@ -1,11 +1,37 @@
-class TrackerViewHelper {
-//    +shipmentID: State<String>
-//    +shipmentTotes:State<String[]>
-//    +shipmentUpdateHistory: State<String[]>
-//    +expectedShipmentDeliveryDate: State<String[]>
-//    +shipmentStatus: State<String>
-//
-//    *all attributes have a private setter in this class
+class TrackerViewHelper(
+    state: State,
+    shipmentID: String,
+    shipmentTotes: MutableList<String>,
+    shipmentUpdateHistory: MutableList<String>,
+    expectedShipmentDeliveryDate: MutableList<String>,
+    shipmentStatus: String
+
+//*all attributes have a private setter in this class
+) {
+    var ID = shipmentID
+        private set(value) {
+            field = value
+        }
+
+    var  Totes = shipmentTotes
+        private set(value) {
+            field = value
+        }
+
+    var History = shipmentUpdateHistory
+        private set(value) {
+            field = value
+        }
+
+    var DeliveryDate = expectedShipmentDeliveryDate
+        private set(value) {
+            field = value
+        }
+
+    var Status = shipmentStatus
+        private set(value) {
+            field = value
+        }
 
 //    fun +previousStatus: String
 //    fun +newStatus: String

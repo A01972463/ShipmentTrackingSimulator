b1f737928dd761596c472976ef335386f2c99c06
Gustavo <a01972463@usu.edu>
2024-07-13 12:24:21 -0600

    Drafting TrackingSimulator

--- src/main/kotlin/TrackingSimulator.kt
+++ src/main/kotlin/TrackingSimulator.kt
@@ -1,3 +1,11 @@
-class TrackingSimulator {
-//    -shipments: Shipment[]
+class TrackingSimulator(
+    shipments: MutableList<Shipment>
+) {
+    var ShipmentList = shipments
+        get() {
+            return field
+        }
+        private set(value) {
+            field = value
+        }
 }
